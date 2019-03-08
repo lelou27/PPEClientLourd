b9a2d8a8736d74feeffe3156de0d85f8b007ab8f
lelou27 <lelou.pro@gmail.com>
2019-03-08 20:02:10 +0100

    Add modif visiteur, reste quelques détails

---
+++ PPEClientLourd/ModificationMyInformations.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace PPEClientLourd
+{
+    partial class ModificationMyInformations
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_nom = new System.Windows.Forms.Label();
+            this.lbl_prenom = new System.Windows.Forms.Label();
+            this.lbl_ville = new System.Windows.Forms.Label();
+            this.lbl_adresse = new System.Windows.Forms.Label();
+            this.lbl_cp = new System.Windows.Forms.Label();
+            this.lbl_secteur = new System.Windows.Forms.Label();
+            this.lbl_labo = new System.Windows.Forms.Label();
+            this.btn_modifier = new System.Windows.Forms.Button();
+            this.btn_retour = new System.Windows.Forms.Button();
+            this.txb_nom = new System.Windows.Forms.TextBox();
+            this.txb_prenom = new System.Windows.Forms.TextBox();
+            this.txb_adresse = new System.Windows.Forms.TextBox();
+            this.txb_ville = new System.Windows.Forms.TextBox();
+            this.txb_cp = new System.Windows.Forms.TextBox();
+            this.cbx_secteur = new System.Windows.Forms.ComboBox();
+            this.cbx_labo = new System.Windows.Forms.ComboBox();
+            this.lbl_error_nom = new System.Windows.Forms.Label();
+            this.lbl_error_secteur = new System.Windows.Forms.Label();
+            this.lbl_error_cp = new System.Windows.Forms.Label();
+            this.lbl_error_ville = new System.Windows.Forms.Label();
+            this.lbl_error_adresse = new System.Windows.Forms.Label();
+            this.lbl_error_prenom = new System.Windows.Forms.Label();
+            this.lbl_error_labo = new System.Windows.Forms.Label();
+            this.lbl_error_general = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_nom
+            // 
+            this.lbl_nom.AutoSize = true;
+            this.lbl_nom.Location = new System.Drawing.Point(30, 44);
+            this.lbl_nom.Name = "lbl_nom";
+            this.lbl_nom.Size = new System.Drawing.Size(38, 13);
+            this.lbl_nom.TabIndex = 0;
+            this.lbl_nom.Text = "Nom : ";
+            // 
+            // lbl_prenom
+            // 
+            this.lbl_prenom.AutoSize = true;
+            this.lbl_prenom.Location = new System.Drawing.Point(30, 86);
+            this.lbl_prenom.Name = "lbl_prenom";
+            this.lbl_prenom.Size = new System.Drawing.Size(49, 13);
+            this.lbl_prenom.TabIndex = 1;
+            this.lbl_prenom.Text = "Prénom :";
+            // 
+            // lbl_ville
+            // 
+            this.lbl_ville.AutoSize = true;
+            this.lbl_ville.Location = new System.Drawing.Point(30, 169);
+            this.lbl_ville.Name = "lbl_ville";
+            this.lbl_ville.Size = new System.Drawing.Size(32, 13);
+            this.lbl_ville.TabIndex = 2;
+            this.lbl_ville.Text = "Ville :";
+            // 
+            // lbl_adresse
+            // 
+            this.lbl_adresse.AutoSize = true;
+            this.lbl_adresse.Location = new System.Drawing.Point(30, 128);
+            this.lbl_adresse.Name = "lbl_adresse";
+            this.lbl_adresse.Size = new System.Drawing.Size(51, 13);
+            this.lbl_adresse.TabIndex = 3;
+            this.lbl_adresse.Text = "Adresse :";
+            // 
+            // lbl_cp
+            // 
+            this.lbl_cp.AutoSize = true;
+            this.lbl_cp.Location = new System.Drawing.Point(30, 205);
+            this.lbl_cp.Name = "lbl_cp";
+            this.lbl_cp.Size = new System.Drawing.Size(69, 13);
+            this.lbl_cp.TabIndex = 4;
+            this.lbl_cp.Text = "Code postal :";
+            // 
+            // lbl_secteur
+            // 
+            this.lbl_secteur.AutoSize = true;
+            this.lbl_secteur.Location = new System.Drawing.Point(30, 250);
+            this.lbl_secteur.Name = "lbl_secteur";
+            this.lbl_secteur.Size = new System.Drawing.Size(50, 13);
+            this.lbl_secteur.TabIndex = 5;
+            this.lbl_secteur.Text = "Secteur :";
+            // 
+            // lbl_labo
+            // 
+            this.lbl_labo.AutoSize = true;
+            this.lbl_labo.Location = new System.Drawing.Point(30, 300);
+            this.lbl_labo.Name = "lbl_labo";
+            this.lbl_labo.Size = new System.Drawing.Size(37, 13);
+            this.lbl_labo.TabIndex = 6;
+            this.lbl_labo.Text = "Labo :";
+            // 
+            // btn_modifier
+            // 
+            this.btn_modifier.Location = new System.Drawing.Point(298, 380);
+            this.btn_modifier.Name = "btn_modifier";
+            this.btn_modifier.Size = new System.Drawing.Size(82, 31);
+            this.btn_modifier.TabIndex = 7;
+            this.btn_modifier.Text = "&Modifier";
+            this.btn_modifier.UseVisualStyleBackColor = true;
+            this.btn_modifier.Click += new System.EventHandler(this.btn_modifier_Click);
+            // 
+            // btn_retour
+            // 
+            this.btn_retour.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_retour.Location = new System.Drawing.Point(210, 380);
+            this.btn_retour.Name = "btn_retour";
+            this.btn_retour.Size = new System.Drawing.Size(82, 31);
+            this.btn_retour.TabIndex = 8;
+            this.btn_retour.Text = "&Retour";
+            this.btn_retour.UseVisualStyleBackColor = true;
+            this.btn_retour.Click += new System.EventHandler(this.btn_retour_Click);
+            // 
+            // txb_nom
+            // 
+            this.txb_nom.Location = new System.Drawing.Point(145, 41);
+            this.txb_nom.Name = "txb_nom";
+            this.txb_nom.Size = new System.Drawing.Size(195, 20);
+            this.txb_nom.TabIndex = 9;
+            // 
+            // txb_prenom
+            // 
+            this.txb_prenom.Location = new System.Drawing.Point(145, 83);
+            this.txb_prenom.Name = "txb_prenom";
+            this.txb_prenom.Size = new System.Drawing.Size(195, 20);
+            this.txb_prenom.TabIndex = 10;
+            // 
+            // txb_adresse
+            // 
+            this.txb_adresse.Location = new System.Drawing.Point(145, 125);
+            this.txb_adresse.Name = "txb_adresse";
+            this.txb_adresse.Size = new System.Drawing.Size(195, 20);
+            this.txb_adresse.TabIndex = 11;
+            // 
+            // txb_ville
+            // 
+            this.txb_ville.Location = new System.Drawing.Point(145, 166);
+            this.txb_ville.Name = "txb_ville";
+            this.txb_ville.Size = new System.Drawing.Size(195, 20);
+            this.txb_ville.TabIndex = 12;
+            // 
+            // txb_cp
+            // 
+            this.txb_cp.Location = new System.Drawing.Point(145, 202);
+            this.txb_cp.Name = "txb_cp";
+            this.txb_cp.Size = new System.Drawing.Size(95, 20);
+            this.txb_cp.TabIndex = 13;
+            // 
+            // cbx_secteur
+            // 
+            this.cbx_secteur.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbx_secteur.FormattingEnabled = true;
+            this.cbx_secteur.Location = new System.Drawing.Point(145, 247);
+            this.cbx_secteur.Name = "cbx_secteur";
+            this.cbx_secteur.Size = new System.Drawing.Size(195, 21);
+            this.cbx_secteur.TabIndex = 14;
+            // 
+            // cbx_labo
+            // 
+            this.cbx_labo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbx_labo.FormattingEnabled = true;
+            this.cbx_labo.Location = new System.Drawing.Point(145, 297);
+            this.cbx_labo.Name = "cbx_labo";
+            this.cbx_labo.Size = new System.Drawing.Size(195, 21);
+            this.cbx_labo.TabIndex = 15;
+            // 
+            // lbl_error_nom
+            // 
+            this.lbl_error_nom.AutoSize = true;
+            this.lbl_error_nom.ForeColor = System.Drawing.Color.Red;
+            this.lbl_error_nom.Location = new System.Drawing.Point(151, 25);
+            this.lbl_error_nom.Name = "lbl_error_nom";
+            this.lbl_error_nom.Size = new System.Drawing.Size(0, 13);
+            this.lbl_error_nom.TabIndex = 16;
+            // 
+            // lbl_error_secteur
+            // 
+            this.lbl_error_secteur.AutoSize = true;
+            this.lbl_error_secteur.ForeColor = System.Drawing.Color.Red;
+            this.lbl_error_secteur.Location = new System.Drawing.Point(151, 231);
+            this.lbl_error_secteur.Name = "lbl_error_secteur";
+            this.lbl_error_secteur.Size = new System.Drawing.Size(0, 13);
+            this.lbl_error_secteur.TabIndex = 17;
+            // 
+            // lbl_error_cp
+            // 
+            this.lbl_error_cp.AutoSize = true;
+            this.lbl_error_cp.ForeColor = System.Drawing.Color.Red;
+            this.lbl_error_cp.Location = new System.Drawing.Point(151, 189);
+            this.lbl_error_cp.Name = "lbl_error_cp";
+            this.lbl_error_cp.Size = new System.Drawing.Size(0, 13);
+            this.lbl_error_cp.TabIndex = 18;
+            // 
+            // lbl_error_ville
+            // 
+            this.lbl_error_ville.AutoSize = true;
+            this.lbl_error_ville.ForeColor = System.Drawing.Color.Red;
+            this.lbl_error_ville.Location = new System.Drawing.Point(151, 150);
+            this.lbl_error_ville.Name = "lbl_error_ville";
+            this.lbl_error_ville.Size = new System.Drawing.Size(0, 13);
+            this.lbl_error_ville.TabIndex = 19;
+            // 
+            // lbl_error_adresse
+            // 
+            this.lbl_error_adresse.AutoSize = true;
+            this.lbl_error_adresse.ForeColor = System.Drawing.Color.Red;
+            this.lbl_error_adresse.Location = new System.Drawing.Point(151, 109);
+            this.lbl_error_adresse.Name = "lbl_error_adresse";
+            this.lbl_error_adresse.Size = new System.Drawing.Size(0, 13);
+            this.lbl_error_adresse.TabIndex = 20;
+            // 
+            // lbl_error_prenom
+            // 
+            this.lbl_error_prenom.AutoSize = true;
+            this.lbl_error_prenom.ForeColor = System.Drawing.Color.Red;
+            this.lbl_error_prenom.Location = new System.Drawing.Point(151, 67);
+            this.lbl_error_prenom.Name = "lbl_error_prenom";
+            this.lbl_error_prenom.Size = new System.Drawing.Size(0, 13);
+            this.lbl_error_prenom.TabIndex = 21;
+            // 
+            // lbl_error_labo
+            // 
+            this.lbl_error_labo.AutoSize = true;
+            this.lbl_error_labo.ForeColor = System.Drawing.Color.Red;
+            this.lbl_error_labo.Location = new System.Drawing.Point(151, 281);
+            this.lbl_error_labo.Name = "lbl_error_labo";
+            this.lbl_error_labo.Size = new System.Drawing.Size(0, 13);
+            this.lbl_error_labo.TabIndex = 22;
+            // 
+            // lbl_error_general
+            // 
+            this.lbl_error_general.AutoSize = true;
+            this.lbl_error_general.ForeColor = System.Drawing.Color.Red;
+            this.lbl_error_general.Location = new System.Drawing.Point(142, 346);
+            this.lbl_error_general.Name = "lbl_error_general";
+            this.lbl_error_general.Size = new System.Drawing.Size(0, 13);
+            this.lbl_error_general.TabIndex = 23;
+            // 
+            // ModificationMyInformations
+            // 
+            this.AcceptButton = this.btn_modifier;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btn_retour;
+            this.ClientSize = new System.Drawing.Size(392, 423);
+            this.Controls.Add(this.lbl_error_general);
+            this.Controls.Add(this.lbl_error_labo);
+            this.Controls.Add(this.lbl_error_prenom);
+            this.Controls.Add(this.lbl_error_adresse);
+            this.Controls.Add(this.lbl_error_ville);
+            this.Controls.Add(this.lbl_error_cp);
+            this.Controls.Add(this.lbl_error_secteur);
+            this.Controls.Add(this.lbl_error_nom);
+            this.Controls.Add(this.cbx_labo);
+            this.Controls.Add(this.cbx_secteur);
+            this.Controls.Add(this.txb_cp);
+            this.Controls.Add(this.txb_ville);
+            this.Controls.Add(this.txb_adresse);
+            this.Controls.Add(this.txb_prenom);
+            this.Controls.Add(this.txb_nom);
+            this.Controls.Add(this.btn_retour);
+            this.Controls.Add(this.btn_modifier);
+            this.Controls.Add(this.lbl_labo);
+            this.Controls.Add(this.lbl_secteur);
+            this.Controls.Add(this.lbl_cp);
+            this.Controls.Add(this.lbl_adresse);
+            this.Controls.Add(this.lbl_ville);
+            this.Controls.Add(this.lbl_prenom);
+            this.Controls.Add(this.lbl_nom);
+            this.Name = "ModificationMyInformations";
+            this.Text = "Modifier mes informations";
+            this.Load += new System.EventHandler(this.ModificationMyInformations_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_nom;
+        private System.Windows.Forms.Label lbl_prenom;
+        private System.Windows.Forms.Label lbl_ville;
+        private System.Windows.Forms.Label lbl_adresse;
+        private System.Windows.Forms.Label lbl_cp;
+        private System.Windows.Forms.Label lbl_secteur;
+        private System.Windows.Forms.Label lbl_labo;
+        private System.Windows.Forms.Button btn_modifier;
+        private System.Windows.Forms.Button btn_retour;
+        private System.Windows.Forms.TextBox txb_nom;
+        private System.Windows.Forms.TextBox txb_prenom;
+        private System.Windows.Forms.TextBox txb_adresse;
+        private System.Windows.Forms.TextBox txb_ville;
+        private System.Windows.Forms.TextBox txb_cp;
+        private System.Windows.Forms.ComboBox cbx_secteur;
+        private System.Windows.Forms.ComboBox cbx_labo;
+        private System.Windows.Forms.Label lbl_error_nom;
+        private System.Windows.Forms.Label lbl_error_secteur;
+        private System.Windows.Forms.Label lbl_error_cp;
+        private System.Windows.Forms.Label lbl_error_ville;
+        private System.Windows.Forms.Label lbl_error_adresse;
+        private System.Windows.Forms.Label lbl_error_prenom;
+        private System.Windows.Forms.Label lbl_error_labo;
+        private System.Windows.Forms.Label lbl_error_general;
+    }
+}
