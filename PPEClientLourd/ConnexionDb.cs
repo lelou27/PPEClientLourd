62de490f209f39a10f6591946012f779a1097788
lelou27 <lelou.pro@gmail.com>
2019-03-15 11:44:56 +0100

    change connexion

--- PPEClientLourd/ConnexionDb.cs
+++ PPEClientLourd/ConnexionDb.cs
@@ -8,6 +8,7 @@ namespace PPEClientLourd
 {
     public static class ConnexionDb
     {
-        public static string chaineConnexion = "SERVER=172.23.221.36; DATABASE=applicationppe; UID=userApplicationPpe; PASSWORD=userApplicationPpePassword;SslMode=none";  //ceci permettra la connexion à la base de données	Mysql
+        //public static string chaineConnexion = "SERVER=172.23.221.36; DATABASE=applicationppe; UID=userApplicationPpe; PASSWORD=userApplicationPpePassword;SslMode=none";  //ceci permettra la connexion à la base de données	Mysql
+        public static string chaineConnexion = "SERVER=172.23.221.36; DATABASE=applicationppe; UID=root; PASSWORD=;SslMode=none";  //ceci permettra la connexion à la base de données	Mysql
     }
 }
